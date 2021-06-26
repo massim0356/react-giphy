af11b6d6cbe386805782e71ceaf9c8bccef99318
Massim Shah <massimshah@hotmail.com>
2021-06-26 20:09:53 +0900

    created skeltons for components

---
+++ workshop/src/components/gif.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Gif extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Gif;
---
+++ workshop/src/components/searchBar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// eslint-disable-next-line react/prefer-stateless-function
+class SearchBar extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SearchBar;
