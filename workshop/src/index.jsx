9ba1a6d8e646beb42e578f6ccdc4cd239436ad1b
Massim Shah <massimshah@hotmail.com>
2021-06-26 19:57:55 +0900

    created react component with classes

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,12 +1,25 @@
 import "../assets/stylesheets/application.scss";
 
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const Hello = (props) => {
-  const { name } = props;
-  return <h1>Ayoooo {name}</h1>;
-};
+// Simple React component
+// const Hello = (props) => {
+//   const { name } = props;
+//   return <h1>Ayoooo {name}</h1>;
+// };
+
+// Complex React component - with classes
+// eslint-disable-next-line react/prefer-stateless-function
+class Hello extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    const { name } = this.props;
+    return <h1>Ayoooo {name}</h1>;
+  }
+}
 
 const root = document.querySelector("#root");
 ReactDOM.render(<Hello name="Billy" />, root);
