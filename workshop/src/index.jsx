36ea81b08b85858251ff411c35f04d891b2d32b8
Massim Shah <massimshah@hotmail.com>
2021-06-26 19:51:34 +0900

    made basic react component

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,12 @@
-import '../assets/stylesheets/application.scss';
+import "../assets/stylesheets/application.scss";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = (props) => {
+  const { name } = props;
+  return <h1>Ayoooo {name}</h1>;
+};
+
+const root = document.querySelector("#root");
+ReactDOM.render(<Hello name="Billy" />, root);
